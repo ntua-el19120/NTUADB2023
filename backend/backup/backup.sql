e125cca16f505047335c9ca2b8e36ba8654848b2
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-05-27 17:21:59 +0300

    Add files via upload

---
+++ backend/backup/backup.sql
@@ -0,0 +1,34 @@
+INSERT INTO availability (0, 1, 2, 3) VALUES ;
+
+INSERT INTO book (0, 1, 2, 3, 4, 5, 6, 7) VALUES ;
+
+INSERT INTO book_categories (0, 1) VALUES ;
+
+INSERT INTO book_keywords (0, 1) VALUES ;
+
+INSERT INTO book_writers (0, 1) VALUES ;
+
+INSERT INTO borrowing (0, 1, 2, 3, 4) VALUES ;
+
+INSERT INTO generaladmin (0, 1) VALUES (0, 4);
+
+INSERT INTO loggeduser (0) VALUES (4);
+
+INSERT INTO reservation (0, 1, 2) VALUES ;
+
+INSERT INTO review (0, 1, 2, 3, 4) VALUES ;
+
+INSERT INTO schooladmin (0, 1, 2) VALUES ;
+
+INSERT INTO schoolunit (0, 1, 2, 3, 4, 5, 6) VALUES ;
+
+INSERT INTO student (0, 1, 2, 3, 4, 5, 6, 7, 8) VALUES ;
+
+INSERT INTO teacher (0, 1, 2, 3, 4, 5, 6, 7) VALUES ;
+
+INSERT INTO telephoneschool (0, 1) VALUES ;
+
+INSERT INTO telephoneuser (0, 1) VALUES ;
+
+INSERT INTO users (0, 1, 2, 3) VALUES (1, 'atshidfna', '1010101011', 1), (4, 'atshjkidfna', '1010101011', 1), (5, 'a3tshjkidfna', '1010101011', 1), (7, 'a3tsjhjghghjkidfna', '1010101011', 1), (8, 'a3tsjgjhjhjghghjkidf', '1010101011', 1), (9, 'a3tsjgjhjhjghghjjhgk', '1010101011', 1);
+
