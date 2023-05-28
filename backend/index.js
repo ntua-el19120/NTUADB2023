73d23308f77ca7afc2bb270e52d917e3cbc4ea4a
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-05-28 20:37:00 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,18 @@ app.get('/libq', function (req, res) {
   res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
 
+app.get('/libq/generaladmin/createschool', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/createschool.html'));
+});
+
+app.get('/libq/generaladmin/updateschool/:id', function (req, res) {
+  const schoolId = req.params.id;
+  res.sendFile(path.join(__dirname, '../frontend/updateschool.html'));
+});
+
+
+
+
 // Serve the login page
 const login = require('./login');
 app.use('/libq/login', login);
@@ -21,9 +33,24 @@ app.use('/libq/login', login);
 const backupRouter = require('./GeneralAdmin/backup.js');
 app.use('/libq/backup', backupRouter);
 
+const viewSchools = require('./GeneralAdmin/viewschools.js');
+app.use('/libq/generaladmin/viewschools', viewSchools);
+
+const deleteSchool = require('./GeneralAdmin/deleteschools.js');
+app.use('/libq/generaladmin/deleteschool', deleteSchool);
+
+
+
+const createSchool = require('./GeneralAdmin/createschool.js');
+app.use('/libq/generaladmin/createdschool', createSchool);
+
 
 const RestoreRouter = require('./GeneralAdmin/restore.js');
 app.use('/libq/restore', RestoreRouter);
+
+const updateSchool = require('./GeneralAdmin/updateschool.js');
+app.use('/libq/generaladmin/updateschool', updateSchool);
+
 
 
 app.listen(PORT, () => {
