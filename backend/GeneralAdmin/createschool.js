5e9886dfff7e4e5948d8a8a0c48811b26f7e95f2
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-06-04 02:54:16 +0300

    Update createschool.js

--- backend/GeneralAdmin/createschool.js
+++ backend/GeneralAdmin/createschool.js
@@ -2,11 +2,25 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../connect');
 
-// Handle the login form submission
 // ...
 
 router.post('/', function (req, res) {
   const { Name, Adress_street, Adress_number, Adress_city, Email, SchoolPrinciple, SchoolAdmin } = req.body;
+
+  function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    
+    return result;
+  }
+
+  username1 = generateRandomString(8);
+  password1 = generateRandomString(12);
 
   pool.getConnection(function (err, connection) {
     if (err) {
@@ -21,16 +35,77 @@ router.post('/', function (req, res) {
     `;
 
     connection.query(query, [Name, Adress_street, Adress_number, Adress_city, Email, SchoolPrinciple, SchoolAdmin], (error, results) => {
-      connection.release();
-
       if (error) {
+        connection.release();
         console.error('Error creating school unit:', error);
         res.status(500).json({ error: 'An error occurred while creating the school unit' });
         return;
       }
 
-      // Redirect the user to /libq/generaladmin/viewschools
-      res.redirect('/libq/generaladmin/viewschools');
+      const query2 = `
+        INSERT INTO users (Username, Password, Approved)
+        VALUES (?, ?, ?)
+      `;
+
+      connection.query(query2, [username1, password1, 1], (error, results) => {
+        if (error) {
+          connection.release();
+          console.error('Error creating school unit:', error);
+          res.status(500).json({ error: 'An error occurred while creating the school unit' });
+          return;
+        }
+
+        const query01 = `
+          SELECT Idusers
+          FROM users 
+          WHERE Username = ?
+        `;
+
+        connection.query(query01, [username1], (error, results) => {
+          if (error) {
+            connection.release();
+            console.error('Error creating school unit:', error);
+            res.status(500).json({ error: 'An error occurred while creating the school unit' });
+            return;
+          }
+
+          const IDuser = results[0].Idusers;
+
+          const query11 = `
+            SELECT * FROM Schoolunit
+            WHERE Name = ?
+          `;
+
+          connection.query(query11, [Name], (error, results) => {
+            if (error) {
+              connection.release();
+              console.error('Error creating school unit:', error);
+              res.status(500).json({ error: 'An error occurred while creating the school unit' });
+              return;
+            }
+
+            const IDschool = results[0].IdSchool;
+
+            const query1 = `
+              INSERT INTO schooladmin (IdUsers, IdSchool, Name)
+              VALUES (?, ?, ?)
+            `;
+
+            connection.query(query1, [IDuser, IDschool, SchoolAdmin], (error, results) => {
+              connection.release();
+
+              if (error) {
+                console.error('Error creating school unit:', error);
+                res.status(500).json({ error: 'An error occurred while creating the school unit' });
+                return;
+              }
+
+              // Redirect the user to /libq/generaladmin/viewschools
+              res.redirect('/libq/generaladmin/viewschools');
+            });
+          });
+        });
+      });
     });
   });
 });
