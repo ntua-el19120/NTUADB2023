ff663ae5ead0d4aade7463bfed7508afd06bdfc1
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-06-03 16:47:28 +0300

    Update borrowingsperyear.js

--- backend/GeneralAdmin/borrowingsperyear.js
+++ backend/GeneralAdmin/borrowingsperyear.js
@@ -3,63 +3,41 @@ const router = express.Router();
 const pool = require('../connect');
 
 router.get('/', function (req, res) {
-  pool.getConnection((err, connection) => {
+  const { Category } = req.query;
+
+  pool.getConnection(function (err, connection) {
     if (err) {
-      console.error('Error connecting to database:', err);
-      res.status(500).send('Internal Server Error');
+      console.error('Error getting database connection:', err);
+      res.status(500).json({ error: 'An error occurred while getting a database connection' });
       return;
     }
-    
+
     const query = `
-      SELECT b.*,
-        GROUP_CONCAT(DISTINCT bc.Category SEPARATOR ', ') AS Categories,
-        GROUP_CONCAT(DISTINCT bk.Keyword SEPARATOR ', ') AS Keywords
-      FROM book AS b
-      JOIN availability AS a ON b.ISBN = a.ISBN
-      LEFT JOIN book_categories AS bc ON b.ISBN = bc.ISBN
-      LEFT JOIN book_keywords AS bk ON b.ISBN = bk.ISBN
-      WHERE EXISTS (
-        SELECT 1
-        FROM student AS s
-        WHERE s.IdUsers = 1 AND a.IdSchool = s.IdSchool
-      )
-      OR EXISTS (
-        SELECT 1
-        FROM teacher AS t
-        WHERE t.IdUsers = 1 AND a.IdSchool = t.IdSchool
-      )
-      GROUP BY b.ISBN;
+      SELECT NumBorrowedBooks, GROUP_CONCAT(DISTINCT SchoolAdminName) AS SchoolAdminList
+      FROM (
+        SELECT YEAR(b.BorrowDate) AS Year, p.SchoolAdminName, COUNT(b.ISBN) AS NumBorrowedBooks
+        FROM Persons_Per_School_View p
+        JOIN Borrowing b ON p.PersonId = b.IdUsers
+        GROUP BY YEAR(b.BorrowDate), p.SchoolAdminName
+      ) AS subquery
+      WHERE NumBorrowedBooks > 20
+      GROUP BY NumBorrowedBooks
+      ORDER BY NumBorrowedBooks DESC;
     `;
-    
-    connection.query(query, (error, results) => {
-      connection.release(); // Release the connection
-      
-      if (error) {
-        console.error('Error executing query:', error);
-        res.status(500).send('Internal Server Error');
+
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'An error occurred while executing the query' });
         return;
       }
-      
-      const tableRows = results.map(book => `
-        <tr>
-          <td>${book.ISBN}</td>
-          <td>${book.Title}</td>
-          <td>${book.Publisher}</td>
-          <td>${book.PageNumber}</td>
-          <td>${book.Summary}</td>
-          <td><img src="${book.Picture}" alt="Book Cover" height="100"></td>
-          <td>${book.Language}</td>
-          <td>${book.Rating}</td>
-          <td>${book.Categories}</td>
-          <td>${book.Keywords}</td>
-        </tr>
-      `).join('');
-      
-      const html = `
+
+      // Generate HTML table
+      let htmlTable = `
         <!DOCTYPE html>
         <html>
         <head>
-          <title>Books</title>
+          <title>Query 3.1.5</title>
           <style>
             body {
               font-family: Arial, sans-serif;
@@ -119,32 +97,40 @@ router.get('/', function (req, res) {
           </style>
         </head>
         <body>
-          <h1>Books</h1>
+          <h1>School Admins who have loaned the same number of books in a year with more than 20 loans</h1>
           <table>
             <thead>
               <tr>
-                <th>ISBN</th>
-                <th>Title</th>
-                <th>Publisher</th>
-                <th>Page Number</th>
-                <th>Summary</th>
-                <th>Picture</th>
-                <th>Language</th>
-                <th>Rating</th>
-                <th>Categories</th>
-                <th>Keywords</th>
+                <th>Number of Borrowed Books</th>
+                <th>List of School Admins</th>
               </tr>
             </thead>
             <tbody>
-              ${tableRows}
+      `;
+
+      // Append rows to the HTML table
+      for (const row of results) {
+        htmlTable += `
+          <tr>
+            <td>${row.NumBorrowedBooks}</td>
+            <td>${row.SchoolAdminList}</td>
+          </tr>
+        `;
+      }
+
+      // Complete the HTML table
+      htmlTable += `
             </tbody>
           </table>
-          <a class="back-button" href="/">Back</a>
+          <a href="/libq/generaladmin" class="back-button">Back to the homepage</a>
         </body>
         </html>
       `;
-      
-      res.send(html); // Send the HTML response
+
+      // Send the HTML response
+      res.send(htmlTable);
+
+      connection.release();
     });
   });
 });
